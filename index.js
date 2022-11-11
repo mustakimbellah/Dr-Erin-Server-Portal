8e726d8f45a829e7851b9c062b9adb96f899b230
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-11-11 12:59:50 +0600

    inser added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //     const collection = client.db("test").collection("devices");
 //     // perform actions on the collection object
 //     client.close();
-// });
+// });  https://doctor-server-side.vercel.app/user
 
 
 async function run() {
@@ -35,7 +35,54 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
 
-        })
+        });
+        app.get('/treeServices', async (req, res) => {
+
+            const query = {}
+            const cursor = serviceCollection.find(query);
+
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+
+        });
+
+        app.get('/services/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+
+        });
+
+
+
+
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
+
+
+async function run() {
+    try {
+
+        const userCollection = client.db('doctorDB').collection('review');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user)
+            res.send(result);
+        });
+
+
+
 
     }
     finally {
